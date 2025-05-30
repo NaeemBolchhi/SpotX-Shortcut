d1137c6a5c1458e4297abc54ade11f97f8ffc665
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2025-05-30 17:38:36 +0600

    trial

---
+++ spotter.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -File "spotx.ps1"
--- spotter.ps1
+++ spotter.ps1
@@ -1,7 +1,9 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadCMD = 'https://naeembolchhi.github.io/Update-SpotX/shortcutter.bat'
+$DownloadFILE1 = 'https://naeembolchhi.github.io/Update-SpotX/spotter.bat'
+$DownloadFILE2 = 'https://naeembolchhi.github.io/Update-SpotX/spotx.ps1'
+$DownloadFILE3 = 'https://naeembolchhi.github.io/Update-SpotX/shortcutter.bat'
 $DownloadICON = 'https://naeembolchhi.github.io/Update-SpotX/spotter.ico'
 
 $folderPath = "$env:ProgramData\U3BvdHRlcg"
@@ -9,53 +11,41 @@ if (-not (Test-Path -Path $folderPath)) {
     New-Item -Path $folderPath -ItemType Directory
 }
 
-$FilePath = "$folderPath\spotter.bat"
-$ShellPath = "$folderPath\spotter.ps1"
+$FilePath1 = "$folderPath\spotter.bat"
+$FilePath2 = "$folderPath\spotx.ps1"
+$FilePath3 = "$folderPath\shortcutter.bat"
 $IconPath = "$folderPath\spotter.ico"
-$CMDPath = "$folderPath\shortcutter.bat"
 
-if (Test-Path $FilePath) {
-    $item = Get-Item -LiteralPath $FilePath
+if (Test-Path $FilePath1) {
+    $item = Get-Item -LiteralPath $FilePath1
     $item.Delete()
 }
-if (Test-Path $ShellPath) {
-    $item = Get-Item -LiteralPath $ShellPath
+if (Test-Path $FilePath2) {
+    $item = Get-Item -LiteralPath $FilePath2
+    $item.Delete()
+}
+if (Test-Path $FilePath3) {
+    $item = Get-Item -LiteralPath $FilePath3
     $item.Delete()
 }
 if (Test-Path $IconPath) {
     $item = Get-Item -LiteralPath $IconPath
     $item.Delete()
 }
-if (Test-Path $CMDPath) {
-    $item = Get-Item -LiteralPath $CMDPath
-    $item.Delete()
-}
 
 try {
-    Invoke-WebRequest -Uri $DownloadICON -UseBasicParsing -OutFile $IconPath
-	Invoke-WebRequest -Uri $DownloadCMD -UseBasicParsing -OutFile $CMDPath
-	
-	"@echo off" | Out-File -FilePath $FilePath
-	"powershell -File `"spotter.ps1`"" | Out-File -FilePath $FilePath -Append
-	"pause" | Out-File -FilePath $FilePath -Append
-	
-	"iex" | Out-File -FilePath $ShellPath -NoNewline
-	" `"& { $" | Out-File -FilePath $ShellPath -Append -NoNewline
-	"(" | Out-File -FilePath $ShellPath -Append -NoNewline
-	"iwr" | Out-File -FilePath $ShellPath -Append -NoNewline
-	" -useb '" | Out-File -FilePath $ShellPath -Append -NoNewline
-	"https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run." | Out-File -FilePath $ShellPath -Append -NoNewline
-	"ps1') } " | Out-File -FilePath $ShellPath -Append -NoNewline
-	"-confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify`"" | Out-File -FilePath $ShellPath -Append -NoNewline
-	
+	Invoke-WebRequest -Uri $DownloadICON -UseBasicParsing -OutFile $IconPath
+	Invoke-WebRequest -Uri $DownloadFILE1 -UseBasicParsing -OutFile $FilePath1
+	Invoke-WebRequest -Uri $DownloadFILE2 -UseBasicParsing -OutFile $FilePath2
+	Invoke-WebRequest -Uri $DownloadFILE3 -UseBasicParsing -OutFile $FilePath3
 } catch {
     Write-Error $_
 	Return
 }
 
-if (Test-Path $CMDPath) {
-    Start-Process $CMDPath -Wait
+if (Test-Path $FilePath3) {
+    Start-Process $FilePath3 -Wait
 	
-	$item = Get-Item -LiteralPath $CMDPath
+	$item = Get-Item -LiteralPath $FilePath3
     $item.Delete()
 }
