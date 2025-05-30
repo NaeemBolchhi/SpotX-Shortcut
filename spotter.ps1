70495faf3a9854de64953d76260578347e652ae3
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2025-05-30 16:59:36 +0600

    trial

---
+++ spotter.ps1
@@ -0,0 +1,48 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadCMD = 'https://naeembolchhi.github.io/Update-SpotX/shortcutter.bat'
+$DownloadICON = 'https://naeembolchhi.github.io/Update-SpotX/spotter.ico'
+
+$folderPath = "$env:ProgramData\U3BvdHRlcg"
+if (-not (Test-Path -Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory
+}
+
+$FilePath = "$folderPath\spotter.bat"
+$ShellPath = "$folderPath\spotter.ps1"
+$IconPath = "$folderPath\spotter.ico"
+$CMDPath = "$folderPath\shortcutter.bat"
+
+if (Test-Path $FilePath) {
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
+if (Test-Path $IconPath) {
+    $item = Get-Item -LiteralPath $IconPath
+    $item.Delete()
+}
+if (Test-Path $CMDPath) {
+    $item = Get-Item -LiteralPath $CMDPath
+    $item.Delete()
+}
+
+try {
+    Invoke-WebRequest -Uri $DownloadICON -UseBasicParsing -OutFile $IconPath
+	Invoke-WebRequest -Uri $DownloadCMD -UseBasicParsing -OutFile $CMDPath
+	
+	"@echo off" | Out-File -FilePath $FilePath
+	"powershell -File `"spotter.ps1`"" | Out-File -FilePath $FilePath -Append
+	
+	"iex `"& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify`"" | Out-File -FilePath $ShellPath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $CMDPath) {
+    Start-Process $CMDPath -Wait
+	
+	$item = Get-Item -LiteralPath $CMDPath
+    $item.Delete()
+}
